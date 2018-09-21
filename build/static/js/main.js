75e1e6451dc9eb5739338e999130666a8cde0a16
bargester <mr.terenyak@gmail.com>
2018-09-21 15:52:33 +0300

    Adaptive-main-page

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-    var $btn = $('.down');
+    var $btn = $('.down-wrap');
     $btn.on('click', function(){
         $('html,body').animate({
             scrollTop: $('.intro-s').outerHeight() - $('.header-s').outerHeight()
         }, 1000)
     });
-
+    $('.header-nav__menu').click( function () {
+        $('.header-nav').slideToggle(500);
+    });
+    $('.btn-call, .btn--reg-form').on('click', function () {
+      $('.overlay').fadeIn();
+      $('#reg-form').fadeIn();
+      $('body').addClass('modal-open');
+    });
+    $('.footer-s__link').on('click', function () {
+        $('.overlay').fadeIn();
+        $('#privacy-form').fadeIn();
+        $('body').addClass('modal-open');
+    });
+    $('.form__close, .overlay').on('click', function () {
+        $('.overlay').fadeOut();
+        $('#reg-form').fadeOut();
+        $('#privacy-form').fadeOut();
+        $('body').removeClass('modal-open');
+    });
 });
